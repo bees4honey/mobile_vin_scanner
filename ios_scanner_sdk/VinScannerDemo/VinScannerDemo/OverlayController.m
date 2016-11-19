364028e67df412b18328ab7f9034f60dec588fe4
Nikita Kodenko <kodenkonikita@gmail.com>
2016-11-19 13:43:21 +0700

    Updated .gitignore & XCode project settings. iOS code reformatted

--- ios_scanner_sdk/VinScannerDemo/VinScannerDemo/OverlayController.m
+++ ios_scanner_sdk/VinScannerDemo/VinScannerDemo/OverlayController.m
@@ -18,17 +18,16 @@
     [super viewDidLoad];
 
     if ([self.parentScanner isRunning]) {
-        [self.startButton setTitle: @"start" forState: UIControlStateNormal];
+        [self.startButton setTitle:@"start" forState:UIControlStateNormal];
+    } else {
+        [self.startButton setTitle:@"stop" forState:UIControlStateNormal];
     }
-    else {
-        [self.startButton setTitle: @"stop" forState: UIControlStateNormal];
-    }
-   	
-    if ([AVCaptureDevice defaultDeviceWithMediaType: AVMediaTypeVideo].hasTorch)
+
+    if ([AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo].hasTorch)
         self.ledButton.hidden = NO;
 }
 
-- (void)viewWillAppear: (BOOL) animated {
+- (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     self.codeLabel.hidden = YES;
 }
@@ -38,28 +37,27 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)scanner: (B4HScannerController *) scanner gotCode: (NSString *) scannedCode {
+- (void)scanner:(B4HScannerController *)scanner gotCode:(NSString *)scannedCode {
     NSLog(@"Scanned code: %@", scannedCode);
     self.codeLabel.text = scannedCode;
     self.codeLabel.hidden = NO;
-    [NSObject cancelPreviousPerformRequestsWithTarget: self selector: @selector(hideLabel) object: nil];
+    [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(hideLabel) object:nil];
     // Remove scanned VIN code from screen after 3 seconds
-    [self performSelector: @selector(hideLabel) withObject: nil afterDelay: 3];
+    [self performSelector:@selector(hideLabel) withObject:nil afterDelay:3];
 }
 
 - (void)ledButtonClick {
     NSError *error;
-    
-    AVCaptureDevice *capDevice = [AVCaptureDevice defaultDeviceWithMediaType: AVMediaTypeVideo];
+
+    AVCaptureDevice *capDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     if (capDevice.hasTorch) {
-        [capDevice lockForConfiguration: &error];
-        if (capDevice.torchMode == AVCaptureTorchModeOff && [capDevice isTorchModeSupported: AVCaptureTorchModeOn]) {
+        [capDevice lockForConfiguration:&error];
+        if (capDevice.torchMode == AVCaptureTorchModeOff && [capDevice isTorchModeSupported:AVCaptureTorchModeOn]) {
             capDevice.torchMode = AVCaptureTorchModeOn;
-            [self.ledButton setSelected: YES];
-        }
-        else {
+            [self.ledButton setSelected:YES];
+        } else {
             capDevice.torchMode = AVCaptureTorchModeOff;
-            [self.ledButton setSelected: NO];
+            [self.ledButton setSelected:NO];
         }
         [capDevice unlockForConfiguration];
     }
@@ -69,14 +67,13 @@
     NSLog(@"%d", [self.parentScanner CheckReadyStatus]);
     if ([self.parentScanner isRunning]) {
         [self.parentScanner stopScanning];
-        [self.startButton setTitle: @"start" forState: UIControlStateNormal];
-    }
-    else {
+        [self.startButton setTitle:@"start" forState:UIControlStateNormal];
+    } else {
         [self.parentScanner startScanningWithOrientation:B4HScannerHorizontal];
-        
+
         [self setBackgroundForScanOrientation:B4HScannerHorizontal];
-        
-        [self.startButton setTitle: @"stop" forState: UIControlStateNormal];
+
+        [self.startButton setTitle:@"stop" forState:UIControlStateNormal];
     }
     self.pauseLabel.hidden = !self.pauseLabel.hidden;
 }
@@ -89,15 +86,13 @@
     self.codeLabel.hidden = YES;
 }
 
-- (void)viewDidLayoutSubviews
-{
+- (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
     B4HScannerOrientation scanOrientation = self.parentScanner.scanOrientation;
     [self setBackgroundForScanOrientation:scanOrientation];
 }
 
-- (void) changeScanOrientation
-{
+- (void)changeScanOrientation {
     if (self.parentScanner.scanOrientation == B4HScannerVertical) {
         // set horizontal scan orientation
         self.parentScanner.scanOrientation = B4HScannerHorizontal;
@@ -105,15 +100,15 @@
         // set vertical scan orientation
         self.parentScanner.scanOrientation = B4HScannerVertical;
     }
-    
+
     [self setBackgroundForScanOrientation:self.parentScanner.scanOrientation];
 }
 
 - (void)setBackgroundForScanOrientation:(B4HScannerOrientation)scanOrientation {
     UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
-    BOOL isLandscape = ( (orientation) == UIInterfaceOrientationLandscapeLeft ||
-                        (orientation) == UIInterfaceOrientationLandscapeRight);
-    
+    BOOL isLandscape = ((orientation) == UIInterfaceOrientationLandscapeLeft ||
+            (orientation) == UIInterfaceOrientationLandscapeRight);
+
     if (scanOrientation == B4HScannerHorizontal) {
         if (isLandscape) {
             self.background.image = [UIImage imageNamed:@"backgroundLandscapeHorizontal.png"];
@@ -128,15 +123,5 @@
         }
     }
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- ios_scanner_sdk/VinScannerDemo/VinScannerDemo/ScannerOverlayController.m
+++ ios_scanner_sdk/VinScannerDemo/VinScannerDemo/ScannerOverlayController.m
@@ -14,8 +14,7 @@
 
 @implementation ScannerOverlayController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
@@ -23,25 +22,20 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
--(NSUInteger) supportedInterfaceOrientations
-{
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
     return UIInterfaceOrientationMaskAll;
 }
 
--(BOOL) shouldAutorotate
-{
+- (BOOL)shouldAutorotate {
     return YES;
 }
+
 @end
